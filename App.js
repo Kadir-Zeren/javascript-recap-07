6c58a9e464affe617a23362bfdf13a7a7004d1e2
kadir-zeren <k4dzeren@gmail.com>
2024-11-13 11:35:09 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+let str1 = "This is a string"
+let str2 = 'This is a string'
+let str3 = `This is a strin`
+let str4 = new String("This is a string")
+
+console. log(typeof str1) // string
+console. log(typeof str2) // string
+console. log(typeof str3) // string
+// console. log(typeof_str4) // object
